d5ca26c4e1992cc6be205929232ff67cb66c3e39
shubhamojha98 <shubhamojha.ojha098@gmail.com>
2024-10-22 17:34:29 +0530

    Update ItemList.js

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -8,7 +8,6 @@ const ItemList = ({ items }) => {
           key={items.card.info.id}
           className="p-2 m-2 border-b-2 border-gray-300 text-left flex justify-between"
         >
-          
           <div className="w-9/12">
             <div className="py-2">
               <span>{items.card.info.name}</span>
@@ -23,7 +22,13 @@ const ItemList = ({ items }) => {
             <p className="text-xs">{items.card.info.description}</p>
           </div>
           <div className="w-3/12 p-4">
-          <img className="w-14" src={IMG_CDN_URL + items.card.info.imageId} />
+            <div className="absolute mx-11 mt-16">
+              <button className="p-2 bg-white shadow-lg m-auto rounded-lg">Add+</button>
+            </div>
+            <img
+              className="w-full"
+              src={IMG_CDN_URL + items.card.info.imageId}
+            />
           </div>
         </div>
       ))}
